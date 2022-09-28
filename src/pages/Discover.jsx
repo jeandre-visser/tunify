3ad8b4b17594fd4fc62429beac9e39bfa48a9827
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-28 17:07:04 -0600

    Add browse h2 tag and a drop down select

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,10 +2,19 @@ import { genres } from '../assets/constants';
 import { Loader, SongCard, Error } from '../components';
 
 const Discover = () => {
+  const genreName = 'Pop';
 
   return (
-    <div>
-      
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-4xl text-white text-left">Browse {genreName}</h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className=""
+        >
+        </select>
+      </div>
     </div>
   );
 };
