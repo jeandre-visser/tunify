41d64944babd85a01ae5851dbeda24c1e8963b57
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-28 19:13:11 -0600

    New folder

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f68de90bc2msh0afc2ba5c8e7022p11c4fejsn457033c3b522',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+  }
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+  export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi'
+  })
