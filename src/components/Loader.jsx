a5e10cc8136f55fcc9c6cc96071051a5a149044f
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-28 20:42:13 -0600

    Add logo to loader and pass in title prop

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,10 @@
-const Loader = () => (
-  <div>Loader</div>
+import { loader } from '../assets';
+
+const Loader = ({ title }) => (
+  <div className="w-full flex justify-center items-center flex-col" >
+    <img src={loader} alt="loader" className="ld ld-beat w-20 object-contain" />
+    <h2 className="font-bold text-3xl text-[#FFF] mt-2" >{title || "Loading"}</h2>
+  </div> 
 );
 
 export default Loader;
