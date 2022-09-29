2bcc68788b6b47bcf45ccd9a160c203a6efe3df1
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 14:52:44 -0600

    Fix height

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,9 +5,10 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, index }) => {
 
+  const activeSong = 'Test'; 
   return (
   <div className="flex flex-col w-[240px] p-5 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
-    <div className="relative w-full h-54 group">
+    <div className="relative w-full h-56 group">
       <div className={`absolute inset-0 justify-center items-center bg-black/40 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black/70' : 'hidden'}`}>
 
       </div>
