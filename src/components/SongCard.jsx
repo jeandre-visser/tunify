d54f0096ae8fa4c4b349ad7dc2f7d5fe6f9cca29
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 14:50:50 -0600

    If song is actively playing, then show, otherwise hide

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, index }) => (
-  <div className="flex flex-col w-[240px] p-5 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
+const SongCard = ({ song, index }) => {
 
+  return (
+  <div className="flex flex-col w-[240px] p-5 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
+    <div className="relative w-full h-54 group">
+      <div className={`absolute inset-0 justify-center items-center bg-black/40 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black/70' : 'hidden'}`}>
+
+      </div>
+    </div>
   </div>
-);
+  )
+};
 
 export default SongCard;
