d09c845cdcd665ab8e7cd68465141a5e25307377
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 17:23:54 -0600

    If song is playing show pause button, otherwise show play button

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,7 +1,19 @@
-import { FaPauseCircle, FaPlayCircle } from 'react-icons';
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
  
-const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay, }) => (
-  
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle
+      className="text-gray-200"
+      onClick={handlePause} 
+      size={35}
+    />
+  ) : (
+    <FaPlayCircle 
+    className="text-gray-200"
+    onClick={handlePlay}
+    size={35}
+    />
+  )
 );
 
 export default PlayPause;
