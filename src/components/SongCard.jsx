c355985c82a34d22b26d25d466f74fda76bf6dd2
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 14:44:45 -0600

    Style cards

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,10 +1,12 @@
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PlayPause from './PlayPause';
-import { playPause, setActiveSongs } from '../redux/features/playerSlice';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = () => (
-  
+const SongCard = ({ song, index }) => (
+  <div className="flex flex-col w-[240px] p-5 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
+
+  </div>
 );
 
 export default SongCard;
