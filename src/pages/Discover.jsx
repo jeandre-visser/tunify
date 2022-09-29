3cab2d80659b5310a5799d8f1befeb73e4eef4d4
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-28 19:16:40 -0600

    Remove example from .env

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,10 +11,20 @@ const Discover = () => {
         <select
           onChange={() => {}}
           value=""
-          className="bg-[#FFF] text-black-400 p-3 text-sm mt-5 rounded-lg sm:mt-0"
+          className="bg-[#FFF] text-black-400 p-3 text-sm rounded-lg sm:mt-0 mt-5"
         >
-          {genres.map((genre) => <option>{genre.title}</option>)}
+          {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, index) => (
+         <SongCard
+          key={song.key}
+          song={song}
+          i={index}
+         />
+        ))}
       </div>
     </div>
   );
