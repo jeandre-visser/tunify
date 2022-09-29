5a6cb2a19fc2e9841c41c78379b3ab8d72efcaec
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 14:58:02 -0600

    Add song cover images that is fetched from the shazam api

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -7,11 +7,12 @@ const SongCard = ({ song, index }) => {
 
   const activeSong = 'Test'; 
   return (
-  <div className="flex flex-col w-[240px] p-5 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
+  <div className="flex flex-col w-[240px] p-4 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
     <div className="relative w-full h-56 group">
       <div className={`absolute inset-0 justify-center items-center bg-black/40 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black/70' : 'hidden'}`}>
-
+        <PlayPause />
       </div>
+      <img src={song.images.coverart} alt="song_image" />
     </div>
   </div>
   )
