d190ec3c37cbaa4ba7a8b47042fb7c04441abecb
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-28 19:54:47 -0600

    Add api key back in temporarily

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-  export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi',
-    baseQuery: fetchBaseQuery({
-      baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-      prepareHeaders: (headers) => {
-        headers.set('X-RapidAPI-Key', SHAZAM_RAPID_API_KEY);
-        return headers;
-      },
-    }),
-    endpoints: (builder) => ({
-      getTopCharts: builder.query({ query: '/charts/world' })
-    })
-  });
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'f68de90bc2msh0afc2ba5c8e7022p11c4fejsn457033c3b522');
+      return headers;
+    }, 
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' })
+  })
+})
 
-  export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery } = shazamCoreApi;
