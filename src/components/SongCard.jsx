7261b815b7f888bcce741181024ae0e16dbeb604
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 15:13:15 -0600

    Pass in song, handlePlay and handlePause props to the PlayPause element

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,12 +5,20 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, index }) => {
 
-  const activeSong = 'Test'; 
+  const activeSong = 'mock'; 
+  const handlePauseClick = () => {
+
+  }
+
   return (
   <div className="flex flex-col w-[240px] p-4 bg-[#FFF]/20 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
     <div className="relative w-full h-56 group">
       <div className={`absolute inset-0 justify-center items-center bg-black/40 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black/70' : 'hidden'}`}>
-        <PlayPause />
+        <PlayPause 
+          song={song}
+          handlePlay={handlePlayClick}
+          handlePause={handlePauseClick}
+        />
       </div>
       <img src={song.images.coverart} alt="song_image" />
     </div>
