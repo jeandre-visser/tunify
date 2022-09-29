fd27932c2b4115f7f04bb32d25aebdd2c06ccaf5
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-28 19:24:56 -0600

    Add api key to prepareHeaders and remove options and fetch request

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,21 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': SHAZAM_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-  }
-};
-
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
 
   export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-      baseUrl: 'https://shazam-core.p.rapidapi.com/v1'
+      baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+      prepareHeaders: (headers) => {
+        headers.set('X-RapidAPI-Key', SHAZAM_RAPID_API_KEY);
+        return headers;
+      }
     })
   })
