e6e8ef420e49fb71b79044d7586f0e14537d87a5
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-30 14:54:23 -0600

    Add link to top-charts

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -35,6 +35,9 @@ const TopPlay = () => {
       <div className="flex flex-col w-full">
         <div className="flex flex-row justify-between items-center">
           <h2>Top Charts</h2>
+          <Link to="/top-charts" >
+            <p>Show more</p>
+          </Link>
         </div>
       </div>
     </div>
