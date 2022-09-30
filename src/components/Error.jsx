04d90e35b53fa7d1d257cfc31dc10903f0f580d5
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 21:06:41 -0600

    Center components vertically on page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Error = () => (
-  <div className="w-full flex justify-center items-center">
+  <div className="w-ful h-full flex justify-center items-center">
     <h1 className="font-bold text-3xl text-[#FFF] mt-2">
       Uh oh! Please try again 
     </h1>
