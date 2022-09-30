98b7259b7d96ca2bb8680cf1da0caadd3a45c1de
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-29 19:03:51 -0600

    Set mobileMenuOpen to false by default

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,16 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+import { links } from '../assets/constants';
+import { logo } from '../assets';
+
+ 
+const Sidebar = () => {
+  const [mobileMenuOpen, setModileMenuOpen] = useState(false);
+  
+  return (
+    <div></div>
+  );
+  };
 
 export default Sidebar;
