e4354bc348fe88209404530a28181620f0cef3b5
Jeandre Visser <jeandrev1414@gmail.com>
2022-09-30 14:38:04 -0600

    Import swiper to build a top artists slider

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,4 +1,10 @@
 import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/free-mode';
 
 const TopPlay = () => (
   <div>TopPlay</div>
