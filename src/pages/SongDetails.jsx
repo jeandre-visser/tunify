bcc3a8499ea89c95f58a351dd4f863a73eca551c
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-03 15:43:37 -0600

    Use the song details query hook to fetch the song data

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,11 +2,13 @@ import { useParams } from "react-router-dom";
 import { DetailsHeader, Error, RelatedSongs, Loader } from '../components';
 import { useSelector, useDispatch } from "react-redux";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
   const { songid } = useParams();
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
   return (
     <div className="flex flex-col">
@@ -17,6 +19,9 @@ const SongDetails = () => {
 
       <div className="mb-8">
         <h3 className="text-[#FFF] text-2xl font-bold">Lyrics</h3>
+        <div className="mt-6">
+
+        </div>
       </div>
     </div>
   );
