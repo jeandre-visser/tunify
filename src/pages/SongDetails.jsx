563cd3bf73dbba29dcc4c71f826a41fa49b02066
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-03 15:29:49 -0600

    Import the necassary components and redux hooks to construct details page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
-
+import { DetailsHeader, Error, RelatedSongs, Loader } from '../components';
+import { useSelector, useDispatch } from "react-redux";
 
 const SongDetails = () => {
 
