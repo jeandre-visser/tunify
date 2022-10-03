2f57c1c402337734de257a85b6cd34006f898c3d
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-03 15:36:17 -0600

    Add a lyrics header

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
 import { DetailsHeader, Error, RelatedSongs, Loader } from '../components';
 import { useSelector, useDispatch } from "react-redux";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
 
 const SongDetails = () => {
-
   const { songid } = useParams();
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-  return <div>SongDetails</div>;
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader 
+        artistId={artistId} 
+        songData={songData} 
+      />
+
+      <div className="mb-8">
+        <h3 className="text-[#FFF] text-2xl font-bold">Lyrics</h3>
+      </div>
+    </div>
+  );
 
 }
 export default SongDetails;
