1517d93cd4dc1c4ac8fd31d6c8893e5a8469f6be
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-03 15:28:16 -0600

    import useParams for songid

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,11 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
 
+
+const SongDetails = () => {
+
+  const { songid } = useParams();
+
+  return <div>SongDetails</div>;
+
+}
 export default SongDetails;
