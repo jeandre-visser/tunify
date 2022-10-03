dd02d2b63244d80e2f69b6038fbab18b960b45e6
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-03 16:01:38 -0600

    Create header for whether user clicks on an artist or a song

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,15 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ songData, artistId, artistData }) => (
+  <div className="flex flex-col w-full relative">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28"/>
+    <div className="absolute flex -items-center inset-0">
+      <img 
+        src={artistId ? artistData?.artistData[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart}
+        alt="cover-art"
+      />
+    </div>
+  </div>
 );
 
 export default DetailsHeader;
