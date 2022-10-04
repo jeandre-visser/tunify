81b461569199c371e099fd935cba2612b5b7ae1e
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-04 16:50:04 -0600

    Map through related songs, add appropriate props to SongBar component

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,8 +1,22 @@
 import SongBar from './SongBar';
 
-const RelatedSongs = ({ isPlaying, data, activeSong, handlePauseClick, handlePlayClick }) => (
+const RelatedSongs = ({ isPlaying, data, artistId, activeSong, handlePauseClick, handlePlayClick }) => (
   <div className="flex flex-col">
-    <h1 className="font-bold text-3xl text-[#FFF]">Related Songs</h1>
+    <h1 className="font-bold text-3xl text-[#FFF] underline underline-offset-4">Related Songs</h1>
+    <div className="mt-6 flex flex-col w-full">
+      {data?.map((song, index) => (
+        <SongBar 
+          song={song}
+          index={index}
+          key={`${song.key}-${artistId}`}
+          artistId={artistId}
+          activeSong={activeSong}
+          isPlaying={isPlaying}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+        />
+      ))}
+    </div>
   </div>
 );
 
