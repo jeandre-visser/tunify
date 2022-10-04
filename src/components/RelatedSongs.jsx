8ca6613d6f25fd16f545a80be57c29878d752d40
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-04 16:35:45 -0600

    Pass in props

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,9 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import { SongBar } from './SongBar';
+
+const RelatedSongs = ({ isPlaying, data, activeSong, handlePauseClick, handlePlayClick }) => (
+  <div className="flex flex-col">
+    
+  </div>
 );
 
 export default RelatedSongs;
