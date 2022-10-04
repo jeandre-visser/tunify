73f8f87b3157408c483d0105d44ae1ec07d918ef
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-04 16:37:34 -0600

    Add Related songs header tag

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,8 +1,8 @@
-import { SongBar } from './SongBar';
+import SongBar from './SongBar';
 
 const RelatedSongs = ({ isPlaying, data, activeSong, handlePauseClick, handlePlayClick }) => (
   <div className="flex flex-col">
-    
+    <h1 className="font-bold text-3xl text-[#FFF]">Related Songs</h1>
   </div>
 );
 
