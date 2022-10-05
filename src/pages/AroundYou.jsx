eb12a50fa4fc6387f40be779b436bd9e27cae1dc
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 14:57:49 -0600

    Comment out geolocation api for now

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -11,19 +11,19 @@ const AroundYou = () => {
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
-  useEffect(() => {
-    axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_KFeTf70hmDo9PzuLIvwVHQbLNSQPK`)
-    .then(response => setCountry(response?.data?.location?.country))
-    .catch(error => console.log(error))
-    .finally(() => setLoading(false));
-  }, [country])
+  // useEffect(() => {
+  //   axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_KFeTf70hmDo9PzuLIvwVHQbLNSQPK`)
+  //   .then(response => setCountry(response?.data?.location?.country))
+  //   .catch(error => console.log(error))
+  //   .finally(() => setLoading(false));
+  // }, [country]) 
 
-  if (isFetching && loading) return <Loader title="Loading popular songs in your country"/>;
+  if (isFetching && loading) return <Loader title="Loading songs near you"/>;
   if (error && country) return <Error />;
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-[#FFF] text-3xl text-left mt-8 mb-12">Popular Songs Near You</h2>
+      <h2 className="font-bold text-[#FFF] text-3xl text-left mt-8 mb-12">Popular Songs in <span>Canada</span></h2>
       <div className="flex flex-wrap justify-center sm:justify-start gap-8">
         {data?.map((song, index) => (
           <SongCard 
