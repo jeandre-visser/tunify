1101ef2fcd33fe0f98355804701dc7d490bbeac0
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 15:10:13 -0600

    Change background opacity

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -5,7 +5,7 @@ const ArtistCard = ({ track }) => {
 
   return (
 
-    <div className="flex flex-col w-[250px] bg-[#FFF]/5 p-2 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="flex flex-col w-[250px] bg-[#FFF] p-2 bg-opacity-90 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <img src={track?.images?.coverart} alt="artist" className="w-full h54 rounded-lg" />
     </div>
 
