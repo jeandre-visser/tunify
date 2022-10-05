b5d688b2bf04d8248f4474e228754a349de7c20a
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 17:21:52 -0600

    Dropdown box styling

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,6 +1,6 @@
 import { genres } from '../assets/constants';
 import { Loader, SongCard, Error } from '../components';
-import { useGetTopChartsQuery, useGetSongsByGenreQuery } from '../redux/services/shazamCore';
+import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectGenreListId } from '../redux/features/playerSlice';
 
@@ -16,11 +16,11 @@ const Discover = () => {
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-4xl text-white text-left">Browse {genreName}</h2>
+        <h2 className="font-bold text-4xl text-white text-left">Browse {genreName || 'Pop'}</h2>
         <select
           onChange={(event) => dispatch(selectGenreListId(event.target.value))}
           value={genreListId || 'pop'}
-          className="bg-[#FFF] text-black-400 p-3 text-sm rounded-lg sm:mt-0 mt-5"
+          className="bg-transparent outline outline-2 outline-white text-[#fff] p-3 text-md rounded-lg sm:mt-0 mt-5 focus:bg-[#fff] focus:text-black w-[100px]"
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>
