0bcb808b29fa1e72afdcdae6435d17852ba5d716
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 15:58:20 -0600

    Initial setup for search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GoSearch } from "react-icons/go"
+
 const Searchbar = () => (
-  <div>Loader</div>
+  <form autocomplete="off"className="p-2 text-gray-400 focus-within:text-gray-600">
+    <label htmlFor="search-field" className="sr-only">
+      Search songs
+    </label>
+    <div className="flex flex-row justify-start items-center">
+
+    </div>
+  </form>
 );
 
 export default Searchbar;
