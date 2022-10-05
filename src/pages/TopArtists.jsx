0dc0a354f02e2e08cb48b062b4bcafb1c2396d0a
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 15:02:08 -0600

    Copy paste top charts to top artists component

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,29 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { ArtistCard, Loader, Error, SongCard } from '../components';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
+const TopArtists = () => {
+
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading top charts"/>;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-[#FFF] text-3xl text-left mt-8 mb-12">Browse Top Artists</h2>
+      <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+        {data?.map((song, index) => (
+          <SongCard 
+            key={song.key}
+            data={data}
+            index={index}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
+        ))}
+      </div>
+    </div>
+ );
+}
 export default TopArtists;
