c966b86fbcb8da5c94bd5ca8133dc815d4d79161
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 14:55:30 -0600

    Copy paste the around you component into the topcharts component

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,35 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Loader, Error, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
-export default TopCharts;
+const AroundYou = () => {
+
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+  if (isFetching && loading) return <Loader title="Loading popular songs in your country"/>;
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-[#FFF] text-3xl text-left mt-8 mb-12">Popular Songs in <span>Canada</span></h2>
+      <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+        {data?.map((song, index) => (
+          <SongCard 
+            key={song.key}
+            data={data}
+            index={index}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
+        ))}
+      </div>
+    </div>
+ );
+}
+export default AroundYou;
