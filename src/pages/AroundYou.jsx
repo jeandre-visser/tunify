5e286640bc82f1a52948bd5e03fb018f34b3b1f2
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-04 19:54:33 -0600

    Import get songs by country query and get the data from a country using the hook

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { Loader, Error, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const AroundYou = () => {
 
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
     axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_KFeTf70hmDo9PzuLIvwVHQbLNSQPK`)
