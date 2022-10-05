5a8ff64a121093344d54ac9e8626a9b813a46566
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 15:28:42 -0600

    Change background

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -5,8 +5,9 @@ const ArtistCard = ({ track }) => {
 
   return (
 
-    <div className="flex flex-col w-[250px] bg-[#FFF] p-2 bg-opacity-90 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <img src={track?.images?.coverart} alt="artist" className="w-full h54 rounded-lg" />
+    <div className="flex flex-col w-[200px] bg-[black] p-3 bg-opacity-40 backdrop-blur-sm animate-slideup rounded-md cursor-pointer">
+      <img src={track?.images?.coverart} alt="artist" className="w-full h-54 rounded-lg" />
+      <p className="mt-3 font-semibold text-lg text-[#fff] truncate">{track?.subtitle}</p>
     </div>
 
   );
