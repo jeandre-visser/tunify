0b9b0fa7dbbbb9b2f07445192968fbe5acafa51c
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-06 16:00:37 -0600

    Add n to contain

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { loader } from '../assets';
 
 const Loader = ({ title }) => (
   <div className="w-full h-full flex justify-center items-center flex-col" >
-    <img src={loader} alt="loader" className="ld ld-beat w-20 h-20 object-contai mt-10" />
+    <img src={loader} alt="loader" className="ld ld-beat w-20 h-20 object-contain mt-10" />
     <h2 className="font-bold text-3xl text-[#FFF] mt-2" >{title || "Loading"}</h2>
   </div> 
 );
