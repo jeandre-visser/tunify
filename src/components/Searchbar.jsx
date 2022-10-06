20c974c073c87774794b6d429313ec3024705ac7
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-05 19:13:52 -0600

    Change size of search icon

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -12,14 +12,14 @@ const Searchbar = () => {
   }
 
   return (
-  <form onSubmit={handleSubmit} autocomplete="off"className="p-2 text-gray-400 focus-within:text-gray-600">
+  <form onSubmit={handleSubmit} autocomplete="off" className="p-1 text-gray-400 focus-within:text-gray-600">
     <label htmlFor="search-field" className="sr-only">
       Search songs
     </label>
     <div className="flex flex-row justify-start items-center">
-      <GoSearch className="w-8 h-8 ml-4 text-gray-300" />
+      <GoSearch className="w-6 h-6 ml-4 text-gray-300" />
       <input 
-      className="flex-1 bg-transparent border-none outline-none text-[#fff] text-base placeholder-gray-300 p-6"
+        className="flex-1 bg-transparent border-none outline-none text-[#fff] text-base placeholder-gray-300 p-6"
         name="search-field"
         id="search-field"
         type="search"
