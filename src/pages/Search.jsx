f3faddc4e4b35d6a8a859ba1977da243a3952e16
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-06 14:33:50 -0600

    Change searchName to searchTerm to fix conflict with state

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,18 +4,19 @@ import { useGetSongsBySearchQuery } from '../redux/services/shazamCore';
 import { useParams } from 'react-router-dom';
 
 const Search = () => {
-  const { searchName } = useParams();
+  const { searchTerm } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetSongsBySearchQuery(searchName);
+  const { data, isFetching, error } = useGetSongsBySearchQuery(searchTerm);
 
-  if (isFetching) return <Loader title="Loading Results"/>;
+  if (isFetching) return <Loader title={`Searching "${searchTerm}"`}/>;
   if (error) return <Error />;
 
   const songs = data?.tracks?.hits?.map((song) => song.track);
+  console.log(searchTerm)
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-semibold text-[#FFF] text-3xl text-left mt-8 mb-12">Results for <span className="font-bold text-[#fff]">{searchName}</span></h2>
+      <h2 className="font-semibold text-[#FFF] text-3xl text-left mt-8 mb-12">Results for <span className="font-bold text-[#fff]">"{searchTerm}"</span></h2>
       <div className="flex flex-wrap justify-center sm:justify-start gap-6">
         {songs?.map((song, index) => (
           <SongCard 
