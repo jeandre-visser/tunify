1b365de1b4ff8f10fd2701e3dbdddc21d2695fa0
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-06 20:09:21 -0600

    Show bar on all screen sizes

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -6,7 +6,7 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
   const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
 
   return (
-    <div className="hidden sm:flex flex-row items-center">
+    <div className="flex flex-row items-center">
       <button type="button" onClick={() => setSeekTime(appTime - 5)} className="hidden lg:mr-4 lg:block text-white">
         <BsFillSkipBackwardFill className="xs:hidden sm:block" />
       </button>
@@ -18,7 +18,7 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
         min={min}
         max={max}
         onInput={onInput}
-        className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
+        className="xs:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
       />
       <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
       <button type="button" onClick={() => setSeekTime(appTime + 5)} className="hidden lg:ml-4 lg:block text-white">
