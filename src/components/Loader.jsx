41b0abaecd0de3a1a6010b09e7a1d4929e381033
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-06 20:30:28 -0600

    Increase space above loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,8 @@
 import { loader } from '../assets';
 
 const Loader = ({ title }) => (
-  <div className="w-full h-full flex justify-center items-center flex-col" >
-    <img src={loader} alt="loader" className="ld ld-beat w-20 h-20 object-contain mt-10" />
+  <div className="w-full h-full flex flex-col justify-center items-center" >
+    <img src={loader} alt="loader" className="ld ld-beat w-20 h-20 object-contain mt-20 mb-8" />
     <h2 className="font-bold text-3xl text-[#FFF] mt-2" >{title || "Loading"}</h2>
   </div> 
 );
