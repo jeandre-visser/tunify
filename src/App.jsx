5e200ed16f08843f78e0481c2bef0ed094082ccc
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-06 20:54:17 -0600

    Alter bg gradient and music player animation

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
       </div>
 
       {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+        <div className="absolute h-24 bottom-0 left-0 right-0 flex animate-slideleft bg-gradient-to-br from-[#620dec]/80 to-[#00000d] backdrop-blur-lg z-10">
           <MusicPlayer />
         </div>
       )}
