1a6cdc33790a64f037cbee854da1b4c435a1a642
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-15 20:16:12 -0600

    Set a max-width for the track playing so there is no overflow

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Track = ({ isPlaying, isActive, activeSong }) => (
-  <div className="flex-1 flex items-center justify-start">
+  <div className="flex-1 flex items-center justify-start max-w-[169px] sm:max-w-full">
     <div className={`${isPlaying && isActive ? 'ld ld-breath w-20 h-20 object-contain' : ''} hidden sm:block h-16 w-16 mr-4`}>
       <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
     </div>
